3e65a4c67e1f5a527ed6929a26c2a22e74c3990d
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-08-12 12:02:32 +0600

    arithmetic operators

---
+++ operators.php
@@ -0,0 +1,13 @@
+<?php
+    $a = 10;
+    $b = 2;
+
+    // arithmetic operators
+    echo $a+$b , "<br>";
+    echo $a-$b , "<br>";
+    echo $a*$b, "<br>";
+    echo $a/$b, "<br>";
+    echo $a%$b, "<br>";
+    echo $a**$b, "<br>"
+    
+?>
